7bab3942035c29405f033406a3822e40593a4e7b
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2025-02-23 13:19:11 +0200

    Delete kata directory

--- kata/smallest.py
+++
@@ -1,8 +0,0 @@
-def smallest(numbers):
-    smallest_num = 0
-
-    for number in numbers:
-        if number < smallest_num:
-            smallest_num = number
-    smallest_num = sorted(numbers)[0]
-    return smallest_num
